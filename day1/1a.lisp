3b682905468d9aff03c17b553a6668f0c7ce5af8
David Barringer <davidbarringer@aol.com>
2020-12-01 10:31:04 +0000

    Added optimised solutions and comments

--- day1/1a.lisp
+++ day1/1a.lisp
@@ -1,3 +1,5 @@
+;;; Loops through each element in the list, comparing it with each element after it.
+;;; Returns a list with nils and a single solution as a dotted pair.
 (defun check-sum (l)
   (if (= (length l) 1) nil (cons (loop for x in (CDR l) if (= 2020 (+ (CAR l) x)) append (cons (CAR l) x)) (check-sum (CDR l)))))
 
@@ -7,3 +9,16 @@
 (defun start ()
   (setq l (CAR (remove nil (check-sum (get-file "day1/day1.csv")))))
   (* (CAR l) (CDR l)))
+
+;;; An optimised solution. Sorts into two lists (one low-to-high, one high-to-low) the first elements of each are added together.
+;;; If the result is less than 2020, remove element from lth lists; if it is greater remove element from htl list.
+;;; When the result is equal to 2020, a dotted pair list is returned
+(defun check-sum-o (l1 l2)
+  (cond ((< 2020 (+ (CAR l1) (CAR l2))) (check-sum-o l1 (CDR l2)))
+        ((> 2020 (+ (CAR l1) (CAR l2))) (check-sum-o (CDR l1) l2))
+        ((= 2020 (+ (CAR l1) (CAR l2))) (cons (CAR l1) (CAR l2)))))
+
+(defun start-o ()
+  (setq l (sort (get-file "day1/day1.csv") #'<))
+  (setq r (check-sum-o l (reverse l)))
+  (* (CAR r) (CDR r)))
--- day1/1b.lisp
+++ day1/1b.lisp
@@ -1,3 +1,6 @@
+;;; Iterates through the list checking each item with each pair that appears after it for a sum.
+;;; Returns a list with nils and two correct solutions in a single list.
+;;; After removing the nils, the first 3 elements (i.e. a solution) are multiplied together.
 (defun check-sum (l)
   (if (= (length l) 1) nil (cons (loop for x in (CDR l) append (CAR (loop for y in (CDDR l) if (= 2020 (+ (CAR l) x y)) collect (list (CAR l) x y)))) (check-sum (CDR l)))))
 
@@ -7,3 +10,18 @@
 (defun start ()
   (setq l (CAR (remove nil (check-sum (get-file "day1/day1.csv")))))
   (* (CAR l) (CADR l) (CADDR l)))
+
+;;; These functions are an optimised solution, iterates through one list as normal, subtracting the item from 2020
+;;; the other lists are sorted (one low-to-high, one high-to-low) sum the first element of each and compare to the result above.
+;;; If it is less, remove element from lth list; if it is more, remove element from htl list.
+;;; Returns a list with nils and 3 combinations of the correct solution.
+(defun check-sum-o (l1 l2 l3)
+  (cond ((OR (eq nil l1) (eq nil l2)) nil)
+        ((< (- 2020 l3) (+ (CAR l1) (CAR l2))) (check-sum-o l1 (CDR l2) l3))
+        ((> (- 2020 l3) (+ (CAR l1) (CAR l2))) (check-sum-o (CDR l1) l2 l3))
+        ((= (- 2020 l3) (+ (CAR l1) (CAR l2))) (list (CAR l1) (CAR l2) l3))))
+
+(defun start-o ()
+  (setq l (sort (get-file "day1/day1.csv") #'<))
+  (setq r (CAR (remove nil (loop for x in l collect (check-sum-o l (reverse l) x)))))
+  (apply '* r))
