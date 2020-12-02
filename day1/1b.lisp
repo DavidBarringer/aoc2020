1194d504d139f64785bec9ad4c801b546134a107
David Barringer <davidbarringer@aol.com>
2020-12-02 16:40:26 +0000

    Added benchmarking tools

--- day1/1b.lisp
+++ day1/1b.lisp
@@ -4,7 +4,7 @@
 (defun check-sum (l)
   (if (= (length l) 1) nil
                         (cons (loop for x in (CDR l) append
-                                (CAR (loop for y in (CDDR l) if (= 2020 (+ (CAR l) x y)) collect (list (CAR l) x y)))) 
+                                (CAR (loop for y in (CDDR l) if (= 2020 (+ (CAR l) x y)) collect (list (CAR l) x y))))
                               (check-sum (CDR l)))))
 
 (defun start ()
@@ -19,7 +19,7 @@
   (cond ((OR (eq nil l1) (eq nil l2)) nil)
         ((< (- 2020 l3) (+ (CAR l1) (CAR l2))) (check-sum-o l1 (CDR l2) l3))
         ((> (- 2020 l3) (+ (CAR l1) (CAR l2))) (check-sum-o (CDR l1) l2 l3))
-        ((= (- 2020 l3) (+ (CAR l1) (CAR l2))) (list (CAR l1) (CAR l2) l3))))
+        (t (list (CAR l1) (CAR l2) l3))))
 
 (defun start-o ()
   (let ((l (sort (mapcar 'parse-integer (get-file "day1/day1.csv")) #'<)))
--- main.lisp
+++ main.lisp
@@ -6,10 +6,23 @@
 (defun get-file (name)
   (uiop:read-file-lines name))
 
-;;; Used to load a specific day and part (remember that part must be a char)
+;;; Used to load a specific day and part (remember that part must be escaped)
 (defun ld (day part)
-  (setq s (format nil "day~D/~D~C.lisp" day day part))
+  (setq s (format nil "day~D/~D~A.lisp" day day part))
   (load s))
+
+;;; Benchmarking tools
+;;; This function makes it pretty
+(defun bench (func-name runs)
+  (format nil "~Fms" (float (bench-fraction func-name runs))))
+
+;;; This function runs file given amount of times and averages the real times
+(defun bench-fraction (func-name runs)
+  (/ (loop repeat runs sum (let ((t1 (get-internal-real-time)))
+                            (funcall func-name)
+                            (let ((t2 (get-internal-real-time)))
+                              (/ (- t2 t1) internal-time-units-per-second))))
+      runs))
 
 (defun exec ()
   (princ "Enter the day you wish to run: ")
