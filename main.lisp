fee2a3e74e4aaa2601c0c62e09cf47e6a6a51c48
David Barringer <davidbarringer@aol.com>
2020-12-02 16:58:08 +0000

    Improved benchmarking to work with any given function.

--- main.lisp
+++ main.lisp
@@ -8,28 +8,27 @@
 
 ;;; Used to load a specific day and part (remember that part must be escaped)
 (defun ld (day part)
-  (setq s (format nil "day~D/~D~A.lisp" day day part))
-  (load s))
+  (let ((s (format nil "day~D/~D~A.lisp" day day part)))
+    (load s)))
 
 ;;; Benchmarking tools
 ;;; This function makes it pretty
-(defun bench (func-name runs)
-  (format nil "~Fms" (float (bench-fraction func-name runs))))
+(defun bench (func-name runs &optional args)
+  (format nil "~Fms" (float (bench-fraction func-name runs args))))
 
 ;;; This function runs file given amount of times and averages the real times
-(defun bench-fraction (func-name runs)
+(defun bench-fraction (func-name runs &optional args)
   (/ (loop repeat runs sum (let ((t1 (get-internal-real-time)))
-                            (funcall func-name)
+                            (apply func-name args)
                             (let ((t2 (get-internal-real-time)))
                               (/ (- t2 t1) internal-time-units-per-second))))
       runs))
 
 (defun exec ()
   (princ "Enter the day you wish to run: ")
-  (setq ans (write-to-string (read)))
-  (load (concatenate 'string "day" ans "/" ans "a.lisp"))
-  (setq a (format nil "~%~%~%~%~%Part A: ~D~%~%" (start)))
-  (load (concatenate 'string "day" ans "/" ans "b.lisp"))
-  (setq b (format nil "Part B: ~D~%~%~%" (start)))
-  (princ (concatenate 'string a b))
-  t)
+  (let ((ans (write-to-string (read))))
+        (load (concatenate 'string "day" ans "/" ans "a.lisp"))
+        (let ((a (format nil "~%Part A: ~D~%~%" (start))))
+          (load (concatenate 'string "day" ans "/" ans "b.lisp"))
+          (let ((b (format nil "Part B: ~D~%" (start))))
+            (concatenate 'string a b)))))
