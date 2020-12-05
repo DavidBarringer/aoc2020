5bdc38bb5849584658aa983e1c085b2ab18a352d
David Barringer <davidbarringer@aol.com>
2020-12-05 10:05:27 +0000

    Code golfing

--- day5/5a.lisp
+++ day5/5a.lisp
@@ -1,18 +1,10 @@
-;;; Changes a binary string to a decimal integer
-(defun seat-conv (s)
-  (parse-integer s :radix 2))
-
 ;;; Replaces F and L with 0, B and R with 1. There may be a better way to do this
 (defun s-to-bin (s)
   (substitute #\0 #\F (substitute #\0 #\L (substitute #\1 #\B (substitute #\1 #\R s)))))
 
-;;; Each seat is a list of row binary and col binary, converts and sums (multiplying row by 8 first)
-(defun get-ids (seats)
-  (loop for seat in seats collect (+ (* 8 (seat-conv (CAR seat))) (seat-conv (CADR seat)))))
-
-;;; Changes the list of strings into binary, then splits into rows and cols
-(defun parse-input (l)
-  (mapcar (lambda (n) (list (subseq n 0 7) (subseq n 7))) (mapcar 's-to-bin l)))
+;;; Converts each seat from its binary string to a decimal int
+(defun get-ids (l)
+  (mapcar (lambda (s) (parse-integer s :radix 2)) l))
 
 (defun start ()
-  (apply 'max (get-ids (parse-input (get-file "day5/day5.csv")))))
+  (apply 'max (get-ids (mapcar 's-to-bin (get-file "day5/day5.csv")))))
--- day5/5b.lisp
+++ day5/5b.lisp
@@ -1,17 +1,11 @@
-(defun seat-conv (s)
-  (parse-integer s :radix 2))
-
 (defun s-to-bin (s)
   (substitute #\0 #\F (substitute #\0 #\L (substitute #\1 #\B (substitute #\1 #\R s)))))
 
 (defun get-ids (l)
-  (loop for seat in l collect (+ (* 8 (seat-conv (CAR seat))) (seat-conv (CADR seat)))))
-
-(defun parse-input (l)
-  (mapcar (lambda (n) (list (subseq n 0 7) (subseq n 7))) (mapcar 's-to-bin l)))
+  (mapcar (lambda (s) (parse-integer s :radix 2)) l))
 
 ;;; Makes a list from 0 to highest seat id then does a set difference of that with list of seat ids
 ;;; Returns in high-to-low, so the first element is the missing seat
 (defun start ()
-  (let ((ids (get-ids (parse-input (get-file "day5/day5.csv")))))
+  (let ((ids (get-ids (mapcar 's-to-bin (get-file "day5/day5.csv")))))
     (CAR (set-difference (loop for i from 0 to (apply 'max ids) collect i) ids))))
