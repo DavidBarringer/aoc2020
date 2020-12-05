332530823e80c48f3fbb4c4aef3bee4e8ec4a8b3
David Barringer <davidbarringer@aol.com>
2020-12-05 10:15:58 +0000

    Combined two functions

--- day5/5a.lisp
+++ day5/5a.lisp
@@ -1,10 +1,6 @@
-;;; Replaces F and L with 0, B and R with 1. There may be a better way to do this
+;;; Replaces F and L with 0, B and R with 1, then parses the binary string into a decimal integer.
 (defun s-to-bin (s)
-  (substitute #\0 #\F (substitute #\0 #\L (substitute #\1 #\B (substitute #\1 #\R s)))))
-
-;;; Converts each seat from its binary string to a decimal int
-(defun get-ids (l)
-  (mapcar (lambda (s) (parse-integer s :radix 2)) l))
+  (parse-integer (substitute #\0 #\F (substitute #\0 #\L (substitute #\1 #\B (substitute #\1 #\R s)))) :radix 2))
 
 (defun start ()
-  (apply 'max (get-ids (mapcar 's-to-bin (get-file "day5/day5.csv")))))
+  (apply 'max (mapcar 's-to-bin (get-file "day5/day5.csv"))))
--- day5/5b.lisp
+++ day5/5b.lisp
@@ -1,11 +1,8 @@
 (defun s-to-bin (s)
-  (substitute #\0 #\F (substitute #\0 #\L (substitute #\1 #\B (substitute #\1 #\R s)))))
-
-(defun get-ids (l)
-  (mapcar (lambda (s) (parse-integer s :radix 2)) l))
+  (parse-integer (substitute #\0 #\F (substitute #\0 #\L (substitute #\1 #\B (substitute #\1 #\R s)))) :radix 2))
 
 ;;; Makes a list from 0 to highest seat id then does a set difference of that with list of seat ids
 ;;; Returns in high-to-low, so the first element is the missing seat
 (defun start ()
-  (let ((ids (get-ids (mapcar 's-to-bin (get-file "day5/day5.csv")))))
+  (let ((ids (mapcar 's-to-bin (get-file "day5/day5.csv"))))
     (CAR (set-difference (loop for i from 0 to (apply 'max ids) collect i) ids))))
