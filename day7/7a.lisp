53cb0e126cbeaff928b7c675ab0cc967bc0e7729
David Barringer <davidbarringer@aol.com>
2020-12-07 13:11:43 +0000

    Added comments and a little code cleaning

--- day7/7a.lisp
+++ day7/7a.lisp
@@ -1,16 +1,23 @@
+;;; Start by stipping "." and " " from the input, then splits on "contain".
+;;; Then go through that list and makes a list with elements that look like: (colour ((colour . num) ...))
 (defun parse-input (l)
   (loop for b in (mapcar (lambda (s) (split "contain" (remove #\. (remove #\  s)))) l)
     collect (cons (read-from-string (unpluralise (CAR b))) (list (pair-int (split "," (CADR b)))))))
 
+;;; Takes the list of bags contained, makes a dotted pair of colour and number for each and returns a list of them
 (defun pair-int (l)
   (loop for s in l if (NOT (string= s "nootherbags"))
-    collect (cons (read-from-string (unpluralise (remove (digit-char (parse-integer s :junk-allowed t)) s))) (parse-integer s :junk-allowed t))))
+    collect (cons (read-from-string (unpluralise (remove (digit-char (parse-integer s :junk-allowed t)) s)))
+                  (parse-integer s :junk-allowed t))))
 
+;;; Removes "bag(s)" from the end of a string
 (defun unpluralise (s)
-  (string-right-trim "s" s))
+  (string-right-trim "bags" s))
 
-(defun find-all-ancestors (target l)
-  (loop for bag in l if (find target (mapcar 'car (CADR bag))) nconc (cons (CAR bag) (find-all-ancestors (CAR bag) l))))
+;;; Checks the contains list of each bag, if the target bag is there, the bag gets added to the list and gets searched for
+(defun find-containers (target l)
+  (loop for bag in l if (find target (mapcar 'car (CADR bag))) nconc (cons (CAR bag) (find-containers (CAR bag) l))))
 
+;;; Finds each bag containing target and each bag containing that etc. Removes repeats and gets the length of the final list
 (defun start ()
-  (length (remove-duplicates (find-all-ancestors 'shinygoldbag (parse-input (get-file "day7/day7.csv"))))))
+  (length (remove-duplicates (find-containers 'shinygold (parse-input (get-file "day7/day7.csv"))))))
--- day7/7b.lisp
+++ day7/7b.lisp
@@ -7,15 +7,19 @@
     collect (cons (read-from-string (unpluralise (remove (digit-char (parse-integer s :junk-allowed t)) s))) (parse-integer s :junk-allowed t))))
 
 (defun unpluralise (s)
-  (string-right-trim "s" s))
+  (string-right-trim "bags" s))
 
+;;; Function that returns what a given bag contains
 (defun find-bag-contains (target l)
-  (if (null (CADR (nth (position target (mapcar 'car l)) l))) nil (CADR (nth (position target (mapcar 'car l)) l))))
+  (CADR (nth (position target (mapcar 'car l)) l)))
 
+;;; Recursive function for totalling the number of bags in a bag.
+;;; Basically a DFS that multiplies when taking a path and sums when all paths at level are exhausted
 (defun descend (c l)
-  (cond ((null c) 1)
-        (t (+ (descend (CDR c) l) (* (CDAR c) (descend (find-bag-contains (CAAR c) l) l))))))
+  (if (null c) 1 (+ (descend (CDR c) l) (* (CDAR c) (descend (find-bag-contains (CAAR c) l) l)))))
 
+;;; Descend adds 1 when all contained bags are counted (because the 1 is also used for multiplication)
+;;; The 1 gets taken away at the end
 (defun start ()
   (let ((input (parse-input (get-file "day7/day7.csv"))))
-    (- (descend (find-bag-contains 'shinygoldbag input) input) 1)))
+    (- (descend (find-bag-contains 'shinygold input) input) 1)))
