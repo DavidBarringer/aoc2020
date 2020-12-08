4e3388d597bbad0e48cea13e862a7dfbcb409cc0
David Barringer <davidbarringer@aol.com>
2020-12-08 08:01:37 +0000

    Added macro, allowing machines to be named.

--- day8/8b.lisp
+++ day8/8b.lisp
@@ -3,33 +3,29 @@
   (acc 0)
   (pos 0))
 
-(defun make-hhgc (code)
-  (setq hhgc (make-machine :code code)))
+(defmacro make-hhgc (name code)
+  `(setq name (make-machine :code ,code)))
 
 (defun parse-input (l)
   (loop for s in l collect (cons (read-from-string (CAR (split " " s))) (mapcar 'parse-integer (CDR (split " " s))))))
 
-(defun rebuild-machine (code)
-  (make-hhgc code)
-  (run-machine nil))
-
-(defun run-machine (poslist)
-  (cond ((null (nth (machine-pos hhgc) (machine-code hhgc))) (machine-acc hhgc))
-        ((null (find (machine-pos hhgc) poslist))
-          (setf poslist (cons (machine-pos hhgc) poslist))
-          (funcall (CAR (nth (machine-pos hhgc) (machine-code hhgc))) (CDR (nth (machine-pos hhgc) (machine-code hhgc))))
-          (run-machine poslist))
+(defun run-machine (name poslist)
+  (cond ((null (nth (machine-pos name) (machine-code name))) (machine-acc name))
+        ((null (find (machine-pos name) poslist))
+          (setf poslist (cons (machine-pos name) poslist))
+          (funcall (CAR (nth (machine-pos name) (machine-code name))) name (CDR (nth (machine-pos name) (machine-code name))))
+          (run-machine name poslist))
         (t nil)))
 
-(defun nop (in)
-  (setf (machine-pos hhgc) (+ 1 (machine-pos hhgc))))
+(defun nop (name in)
+  (setf (machine-pos name) (+ 1 (machine-pos name))))
 
-(defun acc (in)
-  (setf (machine-acc hhgc) (+ (CAR in) (machine-acc hhgc)))
-  (setf (machine-pos hhgc) (+ 1 (machine-pos hhgc))))
+(defun acc (name in)
+  (setf (machine-acc name) (+ (CAR in) (machine-acc name)))
+  (setf (machine-pos name) (+ 1 (machine-pos name))))
 
-(defun jmp (in)
-  (setf (machine-pos hhgc) (+ (CAR in) (machine-pos hhgc))))
+(defun jmp (name in)
+  (setf (machine-pos name) (+ (CAR in) (machine-pos name))))
 
 (defun replace-jmp (code)
   (loop for i from 0 to (- (length code) 1) if (eq 'jmp (CAR (nth i code)))
@@ -41,4 +37,4 @@
 
 (defun start ()
   (let ((input (parse-input (get-file "day8/day8.csv"))))
-    (CAR (remove nil (loop for c in (nconc (replace-jmp input) (replace-nop input)) collect (rebuild-machine c))))))
+    (CAR (remove nil (loop for c in (nconc (replace-jmp input) (replace-nop input)) collect (run-machine (make-hhgc (gensym) c) nil))))))
