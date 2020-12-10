a423333c48e0c907eaa83e14593eb319ce5d8c9a
David Barringer <davidbarringer@aol.com>
2020-12-10 11:57:19 +0000

    Day 10 commenting.

--- day10/10a.lisp
+++ day10/10a.lisp
@@ -1,10 +1,14 @@
+;;; List of strings to list of ints etc.
 (defun parse-input (l)
   (mapcar 'parse-integer l))
 
+;;; Subtract the list of jolts inluding start from the list of jolts.
+;;; This means that each jolt is subtracted from the one after it.
 (defun get-diff (l)
   (let ((sl (sort l '<)))
    (mapcar '- sl (cons 0 sl))))
 
+;;; Since the final jolt isn't included, add it to the count of 3s before multiplying
 (defun start ()
   (let ((diff-list (get-diff (parse-input (get-file "day10/day10.csv")))))
     (* (count-if (lambda (n) (= 1 n)) diff-list) (+ 1 (count-if (lambda (n) (= 3 n)) diff-list)))))
--- day10/10b.lisp
+++ day10/10b.lisp
@@ -1,23 +1,24 @@
 (defun parse-input (l)
   (mapcar 'parse-integer l))
 
-(defun generate-arrangements (l)
-  (if (= (length l) 1) 1
-    (+ (generate-arrangements (CDR l)) (cond ((>= 3 (+ (CAR l) (CADR l))) (generate-arrangements (cons (+ (CAR l) (CADR l)) (CDDR l))))
-                                             (t 0)))))
+;;; The list of arrangements is off-by-one when there are multiple > 1 arrangements in a row
+;;; This is due to arrangements not accounting for being invalidated by the previous arrangment
 (defun to-comb (l)
   (loop for i in l collect (if (= 1 (length i)) (CAR i) (- (apply '+ i) 1))))
 
+;;; Counts the number of valid jolts that appear after the current one
 (defun count-valid (check val l)
   (cond ((null (CAR l)) val)
         ((>= 3 (- (CAR l) check)) (count-valid check (+ 1 val) (CDR l)))
         (t val)))
 
+;;; Creates a list of the number of valid jolts after the current jolt
 (defun arrangements (tot l)
   (cond ((null l) tot)
         ((> 4 (length l)) (cons (count-valid (CAR l) 0 (CDR l)) tot))
         (t (arrangements (cons (count-valid (CAR l) 0 (subseq l 1 4)) tot) (CDR l)))))
 
+;;; Splits a list into lists where at least one "1" appears in between
 (defun split-on-ones (l clt res)
   (remove nil (cond ((null l) (cons clt res))
                     ((= (CAR l) 1) (split-on-ones (CDR l) nil (cons clt res)))
