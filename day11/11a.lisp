5aa9b48299ee7c5bbe05e063958d095e8e31e9c9
David Barringer <davidbarringer@aol.com>
2020-12-11 13:12:13 +0000

    Changed to use vectors

--- day11/11a.lisp
+++ day11/11a.lisp
@@ -1,33 +1,42 @@
-;;; Takes list of strings, returns list of lists of characters
+(defvar cols)
+
+;;; Takes list of strings, sets cols to a vector of vectors of characters
 (defun parse-input (l)
-  (mapcar (lambda (s) (concatenate 'list s)) l))
+  (setq cols (coerce (mapcar (lambda (s) (concatenate 'vector s)) l) 'vector)))
+
+;;; Makes an empty vector with length = row length
+(defun make-empty-vector ()
+  (make-array (length (aref cols 0)) :fill-pointer 0))
 
 ;;; If we reach the end without any changes, return the result
 ;;; If we reach the end with changes, start again with the result as the new initial list
 ;;; Otherwise calculate current row, then move to next one
-(defun map-col (y initial res changes)
-  (cond ((AND (= y (length initial)) (NOT changes)) res)
-        ((= y (length initial)) (map-col 0 res nil nil))
-        (t (map-col (+ y 1) initial (cons (CDR (map-row 0 y initial nil nil)) res) (OR changes (CAR (map-row 0 y initial nil nil)))))))
+(defun map-col (y res changes)
+  (cond ((AND (= y (length cols)) (NOT changes)) cols)
+        ((= y (length cols)) (setq cols res) (map-col 0 (make-empty-vector) nil))
+        (t (vector-push (CDR (map-row 0 y (make-empty-vector) nil)) res)
+           (map-col (+ y 1) res (OR changes (CAR (map-row 0 y (make-empty-vector) nil)))))))
 
 ;;; If the seat changes, flag "changes" as t, then add change to result
 ;;; Otherwise add the same seat to result
-(defun map-row (x y initial res changes)
-  (cond ((= x (length (CAR initial))) (cons changes res))
-        ((AND (eq #\L (nth x (nth y initial))) (= 0 (occupy-count x y initial)))
-         (map-row (+ x 1) y initial (cons #\# res) t))
-        ((AND (eq #\# (nth x (nth y initial))) (<= 4 (occupy-count x y initial)))
-         (map-row (+ x 1) y initial (cons #\L res) t))
-        (t (map-row (+ x 1) y initial (cons (nth x (nth y initial)) res) changes))))
+(defun map-row (x y res changes)
+  (cond ((= x (length (aref cols 0))) (cons changes res))
+        ((AND (eq #\L (aref (aref cols y) x)) (= 0 (occupy-count x y)))
+         (vector-push #\# res) (map-row (+ x 1) y res t))
+        ((AND (eq #\# (aref (aref cols y) x)) (<= 4 (occupy-count x y)))
+         (vector-push #\L res) (map-row (+ x 1) y res t))
+        (t (vector-push (aref (aref cols y) x) res) (map-row (+ x 1) y res changes))))
 
-;;; Makes coordinates from (-1,-1) to (1,1) if the are (0,0) or move (x,y) out of bounds, discard
+;;; Makes coordinates from (-1,-1) to (1,1) if they are (0,0) or move (x,y) out of bounds, discard
 ;;; Then counts the number of #s seen
-(defun occupy-count (x y initial)
+(defun occupy-count (x y )
   (loop for i from -1 to 1 sum
     (loop for j from -1 to 1 if (AND (NOT (AND (= i 0) (= j 0)))
                                      (AND (<= 0 (+ x i)) (<= 0 (+ y j)))
-                                     (eq #\# (nth (+ x i) (nth (+ y j) initial)))) count j)))
+                                     (AND (> (length (aref cols 0)) (+ x i)) (> (length cols) (+ y j)))
+                                     (eq #\# (aref (aref cols (+ y j)) (+ x i)))) count j)))
 
 ;;; Parse input, map until there are no changes, then count the number of #s on each row, then sum the rows
 (defun start ()
-  (apply '+ (mapcar (lambda (s) (count-if (lambda (e) (eq e #\#)) s)) (map-col 0 (parse-input (get-file "day11/day11.csv")) nil nil))))
+  (parse-input (get-file "day11/day11.csv"))
+  (apply '+ (mapcar (lambda (s) (count-if (lambda (e) (eq e #\#)) s)) (coerce (map-col 0 (make-empty-vector) nil) 'list))))
--- day11/11b.lisp
+++ day11/11b.lisp
@@ -1,38 +1,45 @@
-(defun parse-input (l)
-  (mapcar (lambda (s) (concatenate 'list s)) l))
+(defvar cols)
 
-(defun map-col (y initial res changes)
-  (cond ((AND (= y (length initial)) (NOT changes)) res)
-        ((= y (length initial)) (map-col 0 res nil nil))
-        (t (map-col (+ y 1) initial (cons (CDR (map-row 0 y initial nil nil)) res) (OR changes (CAR (map-row 0 y initial nil nil)))))))
+(defun parse-input (l)
+  (setq cols (coerce (mapcar (lambda (s) (concatenate 'vector s)) l) 'vector)))
+
+(defun make-empty-vector ()
+  (make-array (length (aref cols 0)) :fill-pointer 0))
+
+(defun map-col (y res changes)
+  (cond ((AND (= y (length cols)) (NOT changes)) cols)
+        ((= y (length cols)) (setq cols res) (map-col 0 (make-empty-vector) nil))
+        (t (vector-push (CDR (map-row 0 y (make-empty-vector) nil)) res)
+           (map-col (+ y 1) res (OR changes (CAR (map-row 0 y (make-empty-vector) nil)))))))
 
 ;;; Replace 4 with 5
-(defun map-row (x y initial res changes)
-  (cond ((= x (length (CAR initial))) (cons changes res))
-        ((AND (eq #\L (nth x (nth y initial))) (= 0 (occupy-count x y initial)))
-         (map-row (+ x 1) y initial (cons #\# res) t))
-        ((AND (eq #\# (nth x (nth y initial))) (<= 5 (occupy-count x y initial)))
-         (map-row (+ x 1) y initial (cons #\L res) t))
-        (t (map-row (+ x 1) y initial (cons (nth x (nth y initial)) res) changes))))
+(defun map-row (x y res changes)
+  (cond ((= x (length (aref cols 0))) (cons changes res))
+        ((AND (eq #\L (aref (aref cols y) x)) (= 0 (occupy-count x y)))
+         (vector-push #\# res) (map-row (+ x 1) y res t))
+        ((AND (eq #\# (aref (aref cols y) x)) (<= 5 (occupy-count x y)))
+         (vector-push #\L res) (map-row (+ x 1) y res t))
+        (t (vector-push (aref (aref cols y) x) res) (map-row (+ x 1) y res changes))))
 
 ;;; Checks that (x,y) is still within the bounds of the seat list
-(defun out-of-bounds (x y initial)
-  (OR (> 0 x) (> 0 y) (<= (length initial) y) (<= (length (CAR initial)) x)))
+(defun out-of-bounds (x y)
+  (OR (> 0 x) (> 0 y) (<= (length cols) y) (<= (length (aref cols 0)) x)))
 
 ;;; Moves (x,y) by the gradient, then checks if oob/seat. If floor then step again.
-(defun step-until-seat (x-diff y-diff x y initial)
-  (cond ((out-of-bounds x y initial) 0)
-        ((eq #\# (nth x (nth y initial))) 1)
-        ((eq #\L (nth x (nth y initial))) 0)
-        (t (step-until-seat x-diff y-diff (+ x x-diff) (+ y y-diff) initial))))
+(defun step-until-seat (x-diff y-diff x y)
+  (cond ((out-of-bounds x y) 0)
+        ((eq #\# (aref (aref cols y) x)) 1)
+        ((eq #\L (aref (aref cols y) x)) 0)
+        (t (step-until-seat x-diff y-diff (+ x x-diff) (+ y y-diff)))))
 
 ;;; Makes a list of coordinates from (-1,-1) to (1,1), discarding (0,0)
 (defun get-coords ()
   (loop for i from -1 to 1 nconc (loop for j from -1 to 1 if (NOT (AND (= i 0) (= j 0))) collect (cons i j))))
 
 ;;; Goes through each of the gradients make by get-coords, sums the result of stepping through each
-(defun occupy-count (x y initial)
-  (loop for c in (get-coords) sum (step-until-seat (CAR c) (CDR c) (+ (CAR c) x) (+ (CDR c) y) initial)))
+(defun occupy-count (x y)
+  (loop for c in (get-coords) sum (step-until-seat (CAR c) (CDR c) (+ (CAR c) x) (+ (CDR c) y))))
 
 (defun start ()
-  (apply '+ (mapcar (lambda (s) (count-if (lambda (e) (eq e #\#)) s)) (map-col 0 (parse-input (get-file "day11/day11.csv")) nil nil))))
+  (parse-input (get-file "day11/day11.csv"))
+  (apply '+ (mapcar (lambda (s) (count-if (lambda (e) (eq e #\#)) s)) (coerce (map-col 0 (make-empty-vector) nil) 'list))))
