ad8a45534dbc562f34395d58c3ee7065112db6ac
David Barringer <davidbarringer@aol.com>
2020-12-12 11:39:10 +0000

    Commenting for day 12.

--- day12/12a.lisp
+++ day12/12a.lisp
@@ -1,6 +1,12 @@
+;;; Takes each line and changes it to a dotted pair (char . int)
 (defun parse-input (l)
   (loop for s in l collect (cons (char (subseq s 0 1 ) 0) (parse-integer (subseq s 1)))))
 
+;;; Goes through each step, checking each instruction. N,E,S,W modify x and y directly.
+;;; Facing is defined as 3,2,1,0 for N,E,S,W respectively.
+;;; L and R change the direction the ship is facing by adding/subtracting angle/90 then doing mod 4.
+;;; F checks if the ship is facing N/S or E/W. If E/W, then subtract 1 to get 1/-1, multiply by the distance and add to x.
+;;; If N/S, subtract 2 to get 1/-1, multiply by the distance and add to y.
 (defun move-ship (x y facing steps)
   (cond ((null steps) (cons x y))
         ((eq #\N (CAAR steps)) (move-ship x (+ y (CDAR steps)) facing (CDR steps)))
@@ -9,9 +15,10 @@
         ((eq #\W (CAAR steps)) (move-ship (- x (CDAR steps)) y facing (CDR steps)))
         ((eq #\L (CAAR steps)) (move-ship x y (mod (+ facing (/ (CDAR steps) 90)) 4) (CDR steps)))
         ((eq #\R (CAAR steps)) (move-ship x y (mod (- facing (/ (CDAR steps) 90)) 4) (CDR steps)))
-        ((eq #\F (CAAR steps)) (if (= 0 (mod facing 2)) (move-ship (+ x (* (- facing 1) (CDAR steps))) y facing (CDR steps))
-                                                        (move-ship x (+ y (* (- facing 2) (CDAR steps))) facing (CDR steps))))))
+        ((AND (eq #\F (CAAR steps)) (= 0 mod facing 2)) (move-ship (+ x (* (- facing 1) (CDAR steps))) y facing (CDR steps)))
+        ((eq #\F (CAAR steps)) (move-ship x (+ y (* (- facing 2) (CDAR steps))) facing (CDR steps)))))
 
+;;; Add the absolute values of the returned coordinate pair.
 (defun start ()
   (let ((final (move-ship 0 0 2 (parse-input (get-file "day12/day12.csv")))))
     (+ (abs (CAR final)) (abs (CDR final)))))
--- day12/12b.lisp
+++ day12/12b.lisp
@@ -1,18 +1,24 @@
 (defun parse-input (l)
   (loop for s in l collect (cons (char (subseq s 0 1 ) 0) (parse-integer (subseq s 1)))))
 
+;;; Modified from part a to have waypoint x and y, as well as ship x and y.
+;;; N,E,S,W move the waypoint instead of the ship.
+;;; If L or R and rotation is a multiple of 180, multiply waypoint x and y by 1 - rotation/90 mod 4
+;;; Since rotation is a multiple of 180, this will give 1/-1.
+;;; Otherwise, if L move the waypoint y to waypoint x, make it negative if rotation/90 mod 4 is 3.
+;;; Also move waypoint x to waypoint y, make it negative if rotation/90 mod 4 is 1.
+;;; Do the same for R, except swap the negative cases.
+;;; If F, move the ship by the scaled x and y of the waypoint.
 (defun move-ship (wx wy sx sy steps)
   (cond ((null steps) (cons sx sy))
         ((eq #\N (CAAR steps)) (move-ship wx (+ wy (CDAR steps)) sx sy (CDR steps)))
         ((eq #\E (CAAR steps)) (move-ship (+ wx (CDAR steps)) wy sx sy (CDR steps)))
         ((eq #\S (CAAR steps)) (move-ship wx (- wy (CDAR steps)) sx sy (CDR steps)))
         ((eq #\W (CAAR steps)) (move-ship (- wx (CDAR steps)) wy sx sy (CDR steps)))
-        ((AND (eq #\L (CAAR steps)) (= 0 (mod (/ (CDAR steps) 90) 2)))
+        ((AND (OR (eq #\L (CAAR steps)) (eq #\R (CAAR steps))) (= 0 (mod (/ (CDAR steps) 90) 2)))
          (move-ship (* wx (- 1 (mod (/ (CDAR steps) 90) 4))) (* wy (- 1 (mod (/ (CDAR steps) 90) 4))) sx sy (CDR steps)))
         ((eq #\L (CAAR steps))
          (move-ship (* wy (- (mod (/ (CDAR steps) 90) 4) 2)) (* wx (- 2 (mod (/ (CDAR steps) 90) 4))) sx sy (CDR steps)))
-        ((AND (eq #\R (CAAR steps)) (= 0 (mod (/ (CDAR steps) 90) 2)))
-         (move-ship (* wx (- 1 (mod (/ (CDAR steps) 90) 4))) (* wy (- 1 (mod (/ (CDAR steps) 90) 4))) sx sy (CDR steps)))
         ((eq #\R (CAAR steps))
          (move-ship (* wy (- 2 (mod (/ (CDAR steps) 90) 4))) (* wx (- (mod (/ (CDAR steps) 90) 4) 2)) sx sy (CDR steps)))
         ((eq #\F (CAAR steps)) (move-ship wx wy (+ sx (* (CDAR steps) wx)) (+ sy (* (CDAR steps) wy)) (CDR steps)))))
