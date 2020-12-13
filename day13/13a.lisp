e3c696a0834400012e0ea0e61e78912260b6b268
David Barringer <davidbarringer@aol.com>
2020-12-13 16:24:54 +0000

    Comments for day 13.

--- day13/13a.lisp
+++ day13/13a.lisp
@@ -1,9 +1,12 @@
+;;; Takes the input and makes a list with CAR initial timestamp and CDR bus IDs (removing "x"s)
 (defun parse-input (l)
   (cons (parse-integer (CAR l)) (loop for s in (split "," (CADR l)) if (not (eq #\x (char s 0))) collect (parse-integer s))))
 
+;;; Takes the remainder of initial timestamp/bus ID and subtracts it from the bus ID, giving time until the bus next arrives
 (defun get-next-depart (depart l)
   (loop for bus in l collect (- bus (rem depart bus))))
 
+;;; Multiplies the smallest time-to-bus and multiplies it by the ID at that position in the parsed input
 (defun start ()
   (let ((input (parse-input (get-file "day13/day13.csv"))))
     (let ((next-bus-time (get-next-depart (CAR input) (CDR input))))
--- day13/13b.lisp
+++ day13/13b.lisp
@@ -1,12 +1,19 @@
+;;; This time the "x"s are replaced with nil and the initial timestamp is discarded
 (defun parse-input (l)
   (mapcar (lambda (s) (parse-integer s :junk-allowed t)) (split "," (CADR l))))
 
+;;; When the offset is divided by the bus ID perfectly, the lcm of ID and inc is set to the increment
+;;; When it isn't, the increment is added to the offset. The offset is also increased by 1 any time a constraint is met.
+;;; Because inc is the lcm of all IDs that have had constraints met, the constraints will still be met when offset is increased.
+;;; Because offset is increased by 1 when the current constraint is met, the subsequent bus will depart 1 minute after.
+;;; I couldn't think of a better function name at the time of writing.
 (defun contest (offset inc l)
   (cond ((null l) offset)
         ((null (CAR l)) (contest (+ offset 1) inc (CDR l)))
-        ((= 0 (rem offset (CAR l))) (contest (+ offset 1) (* inc (CAR l)) (CDR l)))
+        ((= 0 (rem offset (CAR l))) (contest (+ offset 1) (lcm inc (CAR l)) (CDR l)))
         (t (contest (+ offset inc) inc l))))
 
+;;; Because the returned offset has the 1 minute gaps for each bus, we take of the length of the bus list
 (defun start ()
   (let ((input (parse-input (get-file "day13/day13.csv"))))
-    (- (contest 1 1 input) (length input))))
+    (- (contest (CAR input) 1 input) (length input))))
