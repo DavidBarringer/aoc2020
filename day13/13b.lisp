d2f761060c28a554c8932508d08c8531b5bc3ac4
David Barringer <davidbarringer@aol.com>
2020-12-13 11:18:42 +0000

    13 b corrected.

--- day13/13b.lisp
+++ day13/13b.lisp
@@ -3,10 +3,10 @@
 
 (defun contest (offset inc l)
   (cond ((null l) offset)
-        ((null (CAR l)) (contest-2 (+ offset 1) inc (CDR l)))
-        ((= 0 (rem offset (CAR l))) (contest-2 (+ offset 1) (* inc (CAR l)) (CDR l)))
-        (t (contest-2 (+ offset inc) inc l))))
+        ((null (CAR l)) (contest (+ offset 1) inc (CDR l)))
+        ((= 0 (rem offset (CAR l))) (contest (+ offset 1) (* inc (CAR l)) (CDR l)))
+        (t (contest (+ offset inc) inc l))))
 
 (defun start ()
   (let ((input (parse-input (get-file "day13/day13.csv"))))
-    (- (contest 1 1 input) (length input))))
+    (contest 1 1 input)))
