2820e02fcfb11a60f50cc4279292b9b05e143b88
David Barringer <davidbarringer@aol.com>
2020-12-14 17:19:45 +0000

    Day 14 commented and added hash map so it's faster.

--- day14/14a.lisp
+++ day14/14a.lisp
@@ -1,17 +1,30 @@
-(defun parse-input (l)
-  (loop for m in (mapcar (lambda (s) (split "mem" s)) (CDR (split "mask" (remove #\[ (remove #\  (remove #\= (remove #\Newline l))))))) collect
-    (list (concatenate 'list (CAR m)) (loop for c in (CDR m) for mem = (split "]" c) collect
-                                        (cons (parse-integer (CAR mem)) (concatenate 'list (format nil "~36,'0B" (parse-integer (CADR mem)))))))))
+(defvar memory)
 
+;;; Takes in a string, and returns ((mask-chars) ((memory-address . (36-bit-binary-chars-value)) ...))
+;;; How? First the "junk" is removed i.e. ("[" "=" " " "\n") then split on "mask"
+;;; In each resulting list, we split on mem. It looks something like ("mask-string" (("address]value") ...)).
+;;; Go through this list, turn the first element into a list of chars put it in a list with:
+;;; Going through the ("address]value") list, split on "]", turn the address into an int and make it into a dotted pair with:
+;;; Turn the value into an int, then format it as a binary string 36 chars long, padding whitespace with 0s, then put into char list
+(defun parse-input (l)
+  (loop for m in (mapcar (lambda (s) (split "mem" s))
+                         (CDR (split "mask" (remove #\[ (remove #\  (remove #\= (remove #\Newline l))))))) collect
+    (list (concatenate 'list (CAR m)) (loop for c in (CDR m) for mem = (split "]" c) collect
+                                        (cons (parse-integer (CAR mem))
+                                              (concatenate 'list (format nil "~36,'0B" (parse-integer (CADR mem)))))))))
+
+;;; For each char in val, mask if mask-char is "X", take the val-char, else take the mask-char
 (defun apply-mask (val mask)
   (remove nil (cond ((null val) nil)
                     ((eq #\X (CAR mask)) (cons (CAR val) (apply-mask (CDR val) (CDR mask))))
                     (t (cons (CAR mask) (apply-mask (CDR val) (CDR mask)))))))
 
+;;; For each (memory . value) pair, put into hash table :key memory, :value base 10 number of the result of apply-mask
 (defun to-memory (l)
-  (loop for grp in (CADR l) collect (cons (CAR grp) (parse-integer (concatenate 'string (apply-mask (CDR grp) (CAR l))) :radix 2))))
+  (loop for grp in (CADR l) collect (setf (gethash (CAR grp) memory)
+                                          (parse-integer (concatenate 'string (apply-mask (CDR grp) (CAR l))) :radix 2))))
 
 (defun start ()
-  (let ((mem (remove-duplicates (apply 'append (mapcar 'to-memory (parse-input (get-file-string "day14/day14.csv"))))
-                                :test (lambda (x y) (= (CAR x) (CAR y))))))
-    (apply '+ (mapcar 'CDR mem))))
+  (setq memory (make-hash-table)) ;New hash table for memory
+  (mapcar 'to-memory (parse-input (get-file-string "day14/day14.csv"))) ;Populate hash table
+  (loop for v being each hash-value in memory sum v)) ;Sum values in hash table
--- day14/14b.lisp
+++ day14/14b.lisp
@@ -1,8 +1,15 @@
-(defun parse-input (l)
-  (loop for m in (mapcar (lambda (s) (split "mem" s)) (CDR (split "mask" (remove #\[ (remove #\  (remove #\= (remove #\Newline l))))))) collect
-    (list (concatenate 'list (CAR m)) (loop for c in (CDR m) for mem = (split "]" c) collect
-                                        (cons (concatenate 'list (format nil "~36,'0B" (parse-integer (CAR mem)))) (parse-integer (CADR mem)))))))
+(defvar memory)
 
+;;; Same as part a, but the memory address is the 36-bit-binary-char-list
+(defun parse-input (l)
+  (loop for m in (mapcar (lambda (s) (split "mem" s))
+                         (CDR (split "mask" (remove #\[ (remove #\  (remove #\= (remove #\Newline l))))))) collect
+    (list (concatenate 'list (CAR m)) (loop for c in (CDR m) for mem = (split "]" c) collect
+                                        (cons (concatenate 'list (format nil "~36,'0B" (parse-integer (CAR mem))))
+                                              (parse-integer (CADR mem)))))))
+
+;;; Variates on part a by having res: a list of results.
+;;; When X is called, a branch is made. The number of lists in res gets doubled, half having 1 added, the other having 0 added
 (defun apply-mask (val mask res)
   (cond ((null val) res)
         ((eq #\X (CAR mask)) (apply-mask (CDR val) (CDR mask) (nconc (mapcar (lambda (s) (cons #\1 s)) res)
@@ -10,11 +17,12 @@
         ((eq #\0 (CAR mask)) (apply-mask (CDR val) (CDR mask) (mapcar (lambda (s) (cons (CAR val) s)) res)))
         (t (apply-mask (CDR val) (CDR mask) (mapcar (lambda (s) (cons (CAR mask) s)) res)))))
 
+;;; Similar to part a, but the addresses from apply-mask are converted to base 10 int before being called as hash key
 (defun to-memory (l)
-  (loop for grp in (CADR l) nconc (mapcar (lambda (l) (cons (parse-integer (concatenate 'string l) :radix 2) (CDR grp)))
+  (loop for grp in (CADR l) collect (mapcar (lambda (s) (setf (gethash (parse-integer (concatenate 'string s) :radix 2) memory) (CDR grp)))
                                             (apply-mask (CAR grp) (CAR l) '(())))))
 
 (defun start ()
-  (let ((mem (remove-duplicates (apply 'append (mapcar 'to-memory (parse-input (get-file-string "day14/day14.csv"))))
-                                :test (lambda (x y) (= (CAR x) (CAR y))))))
-    (apply '+ (mapcar 'CDR mem))))
+  (setq memory (make-hash-table))
+  (mapcar 'to-memory (parse-input (get-file-string "day14/day14.csv")))
+  (loop for v being each hash-value in memory sum v))
