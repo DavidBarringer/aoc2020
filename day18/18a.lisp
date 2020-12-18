c0912cf3a46a31ef1dc31aabf38b68ab6cd60c1c
DavidBarringer <davidbarringer@aol.com>
2020-12-18 14:12:03 +0000

    Day 18 better parser.

--- day18/18a.lisp
+++ day18/18a.lisp
@@ -1,26 +1,6 @@
-;;; Takes any character, makes it a string then makes it into a value
-(defun read-from-char (c)
-  (read-from-string (format nil "~A" c)))
-
-;;; Loop for inside of a parentheses. Collects the values and counts how many chars it passes.
-;;; Returns a list of values inside the parentheses.
-(defun collect-inner (chars res count)
-  (cond ((eq #\) (CAR chars)) (cons (+ count 1) (reverse res)))
-        ((eq #\( (CAR chars)) (collect-inner (nthcdr (CAR (collect-inner (CDR chars) nil 1)) chars)
-                                             (cons (CDR (collect-inner (CDR chars) nil 1)) res)
-                                             (+ count (CAR (collect-inner (CDR chars) nil 1)))))
-        (t (collect-inner (CDR chars) (cons (read-from-char (CAR chars)) res) (+ count 1)))))
-
-;;; Loop for collecting characters until "(" is reached.
-;;; Then runs collect-inner to put characters inside parentheses into a list
-(defun collect-parentheses (chars res)
-  (cond ((eq #\( (CAR chars)) (collect-parentheses (nthcdr (CAR (collect-inner (CDR chars) nil 1)) chars) (cons (CDR (collect-inner (CDR chars) nil 1)) res)))
-        ((null chars) (reverse res))
-        (t (collect-parentheses (CDR chars) (cons (read-from-char (CAR chars)) res)))))
-
-;;; Remove spaces from string, make into a list of characters, then parse using collect-parentheses
+;;; Adds parentheses around the string, then reads the string to make a list of operations
 (defun parse-input (l)
-  (mapcar (lambda (s) (collect-parentheses (concatenate 'list (remove #\Space s)) nil)) l))
+  (mapcar (lambda (s) (read-from-string (concatenate 'string "(" s ")" ))) l))
 
 ;;; If left or right side of equation is a list, evaluate that first.
 ;;; Otherwise do function call on 2nd, 1st, and 3rd elements then add result to list
--- day18/18b.lisp
+++ day18/18b.lisp
@@ -1,20 +1,5 @@
-(defun read-from-char (c)
-  (read-from-string (format nil "~A" c)))
-
-(defun collect-inner (chars res count)
-  (cond ((eq #\) (CAR chars)) (cons (+ count 1) (reverse res)))
-        ((eq #\( (CAR chars)) (collect-inner (nthcdr (CAR (collect-inner (CDR chars) nil 1)) chars)
-                                             (cons (CDR (collect-inner (CDR chars) nil 1)) res)
-                                             (+ count (CAR (collect-inner (CDR chars) nil 1)))))
-        (t (collect-inner (CDR chars) (cons (read-from-char (CAR chars)) res) (+ count 1)))))
-
-(defun collect-parentheses (chars res)
-  (cond ((eq #\( (CAR chars)) (collect-parentheses (nthcdr (CAR (collect-inner (CDR chars) nil 1)) chars) (cons (CDR (collect-inner (CDR chars) nil 1)) res)))
-        ((null chars) (reverse res))
-        (t (collect-parentheses (CDR chars) (cons (read-from-char (CAR chars)) res)))))
-
 (defun parse-input (l)
-  (mapcar (lambda (s) (collect-parentheses (concatenate 'list (remove #\Space s)) nil)) l))
+  (mapcar (lambda (s) (read-from-string (concatenate 'string "(" s ")" ))) l))
 
 ;;; Same as part a, but if function symbol is *, then evaluate the left and right sides first.
 (defun evaluate (l)
